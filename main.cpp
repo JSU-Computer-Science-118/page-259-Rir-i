79809da9a34cd41b350b88ea637bedf337db0de0
Rir-i <107610241+Rir-i@users.noreply.github.com>
2022-11-10 20:21:24 -0600

    Final program for all test cases

--- main.cpp
+++ main.cpp
@@ -11,48 +11,46 @@ using namespace std;
 
 int main()
 {
-    int a, b , c;
+    double a, b , c;
     double d,y,z;
     
-    cout <<" Please enter the coefficient of x^2 ";
+    
     cin >> a ;
     
-    cout << "Please enter the coefficient of x ";
     cin >> b ;
     
-    cout << " Please enter the constant term";
     cin >> c ;
     
-    d = b^2 - (4 * a * c);
-    y = -b + (pow(d,0.5) /(2*a));
-    z = -b - (pow(d,0.5) /(2*a));
+    d = pow(b,2) - (4.0 * a * c);
+    y = (-b + (sqrt(d))) /(2*a);
+    z = (-b - (sqrt(d))) /(2*a);
   
   if (a == 0)
   {
-    cout << "invalid entry";
+    cout << "Invalid entry";
     }
     
    else if (d == 0)
     {
-        cout << "This equation has a single (repeated) root" << endl;
-        cout <<"The root of the equation is" << y << "twice." << endl;
+        
+        cout <<"The single root is " << y <<  endl;
         
     }
     else if (d > 0)
         
     {
-        cout << "This equation has two real roots"<< endl;
-        cout << "The roots of the equation are" << y << "and" << z << endl;
-        
+        cout << "Two real roots"<< endl;
+        cout << "- First root " << y <<  endl;
+        cout << "- Second root " << z << endl;
     }
     
     else if (d < 0)
         
     {
-        cout << " This equation has two complex roots"<< endl;
+        cout << "Complex roots"<< endl;
         
     }
     
     return 0;
 }
-
+     
